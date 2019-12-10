c1bd376e02c2b397345133672786087f0fd44b3e
Igor <igortorres213@gmail.com>
2019-12-10 09:21:01 -0300

    igortcruz Portfolio's

---
+++ script/meujavascript.js
@@ -0,0 +1,7 @@
+$('#title1').fadeIn(2000);
+$('#title1').delay(1800).fadeOut(1000);
+
+$('#title2').delay(2000).fadeIn(1000);
+$('#title2').delay(1800).fadeOut(1000);
+
+$('#main').delay(6000).fadeIn(2000);
